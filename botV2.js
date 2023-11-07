c0507110f66e95c6cd2b12dd10de98a82b5beeae
gabrielrocha-youdb <gabriel.rocha@youdb.com.br>
2023-11-07 16:47:37 -0300

    aah vey sla

---
+++ botV2.js
@@ -0,0 +1,76 @@
+const puppeteer = require('puppeteer');
+
+const urls = [
+  'https://resumotechbrasil.blogspot.com/2023/10/a-ascensao-da-etica-hacker-no-brasil.html',
+  'https://resumotechbrasil.blogspot.com/2023/10/gigantes-tech-arrasam-nos-lucros.html',
+  'https://resumotechbrasil.blogspot.com/2023/10/o-obscuro-submundo-dos-engajamentos.html',
+  'https://resumotechbrasil.blogspot.com/2023/10/desvendando-codificacao-base64.html',
+  
+];
+
+const intervaloEntreAcessos = 1000; 
+const maxCliquesPorPagina = 15; 
+
+const userAgents = [
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/16.16299',
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/100.0',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Safari/14.0.3',
+];
+
+function getRandomUserAgent() {
+  const randomIndex = Math.floor(Math.random() * userAgents.length);
+  return userAgents[randomIndex];
+}
+
+async function simularAcesso() {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  // Aumenta o tempo limite para a execução de scripts para evitar o erro
+  await page.setDefaultNavigationTimeout(0);
+  await page.setDefaultTimeout(0); // Aumenta o tempo limite para a execução de scripts
+
+  // Configuração do User-Agent
+  await page.setUserAgent(getRandomUserAgent());
+
+  console.log('Iniciando simulação de acesso.');
+
+  while (true) {
+    for (const url of urls) {
+      try {
+        console.log(`Acessando página: ${url}`);
+        await page.goto(url);
+
+        // Simula cliques aleatórios entre 1 e maxCliquesPorPagina
+        const numCliques = Math.floor(Math.random() * maxCliquesPorPagina) + 1;
+        console.log(`Realizando ${numCliques} cliques na página.`);
+        for (let i = 0; i < numCliques; i++) {
+          const x = Math.random() * (1920 - 1) + 1; // Gere coordenadas x aleatórias
+          const y = Math.random() * (1080 - 1) + 1; // Gere coordenadas y aleatórias
+          await page.mouse.click(x, y);
+        }
+
+        // Aguarde 30 segundos até o próximo acesso
+        console.log(`Aguardando ${intervaloEntreAcessos / 1000} segundos até o próximo acesso...`);
+        await page.waitForTimeout(intervaloEntreAcessos);
+      } catch (error) {
+        // Captura o erro e continua a execução do código
+        console.log(`Ocorreu um erro ao acessar a página: ${url}`);
+        console.log(error);
+        continue;
+      }
+    }
+  }
+}
+
+console.log('Iniciando o aplicativo.');
+
+// Execute a função simularAcesso a cada 3 minutos (180000 milissegundos)
+/* setInterval(() => {
+  console.log('Reiniciando simulação de acesso.');
+  simularAcesso();
+}, 180000); // 180000 milissegundos = 3 minutos
+ */
+// Inicialize a simulação de acesso pela primeira vez
+simularAcesso();
--- src/bot/testSilas.js
+++ src/bot/testSilas.js
@@ -1,12 +1,15 @@
 const puppeteer = require('puppeteer');
 
 const urls = [
-  // urls
+  'https://leidimaia.blogspot.com/',
+  'https://leidimaia.blogspot.com/2023/11/como-ganhar-dinheiro-com-fotografia.html',
+  'https://leidimaia.blogspot.com/',
+  'https://leidimaia.blogspot.com/2023/11/como-ganhar-dinheiro-com-fotografia.html',
+  'https://leidimaia.blogspot.com/',
 ];
 
 const maxCliquesPorPagina = 15;
-const numSimultaneousPages = 5; // Altere para o número desejado de páginas acessadas simultaneamente
-
+const numSimultaneousPages = 5;
 const userAgents = [
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/16.16299',
@@ -14,33 +17,40 @@ const userAgents = [
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Safari/14.0.3',
 ];
 
-
 function getRandomUserAgent() {
   const randomIndex = Math.floor(Math.random() * userAgents.length);
   return userAgents[randomIndex];
 }
 
+function getCurrentTimestamp() {
+  const now = new Date();
+  const timestamp = now.toISOString().slice(0, 19).replace("T", " ");
+  return `[${timestamp}]`;
+}
+
 async function simularAcesso(url) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(0);
-  await page.setDefaultTimeout(0);
+  page.setDefaultNavigationTimeout(0);
+  page.setDefaultTimeout(0);
   await page.setUserAgent(getRandomUserAgent());
 
   try {
-    console.log(`Acessando página: ${url}`);
+    const timestamp = getCurrentTimestamp();
+    console.log(`${timestamp} Acessando página: ${url}`);
     await page.goto(url);
 
     const x = 1920 / 2;
     const y = 1080 / 2;
     const numCliques = Math.floor(Math.random() * maxCliquesPorPagina) + 1;
 
-    console.log(`Realizando ${numCliques} cliques no centro da tela.`);
+    console.log(`${timestamp} Realizando ${numCliques} cliques no centro da tela.`);
     for (let i = 0; i < numCliques; i++) {
       await page.mouse.click(x, y);
     }
   } catch (error) {
-    console.log(`Ocorreu um erro ao acessar a página: ${url}`);
+    const timestamp = getCurrentTimestamp();
+    console.log(`${timestamp} Ocorreu um erro ao acessar a página: ${url}`);
     console.error(error);
   } finally {
     await browser.close();
@@ -49,25 +59,24 @@ async function simularAcesso(url) {
 
 console.log('Iniciando o aplicativo.');
 
-// Função assíncrona que contém o loop infinito
 async function iniciarSimulacao() {
   while (true) {
     const promises = urls.slice(0, numSimultaneousPages).map(url => simularAcesso(url));
 
     await Promise.all(promises)
       .then(() => {
-        console.log('Simulação de acesso concluída para as páginas atuais.');
+        const timestamp = getCurrentTimestamp();
+        console.log(`${timestamp} Simulação de acesso concluída para as páginas atuais.`);
       })
       .catch((error) => {
         console.error('Erro durante a simulação de acesso:', error);
       });
 
-    // Aguarde um intervalo antes de iniciar o próximo conjunto de acessos
-    const intervaloEntreAcessos = 2000; // em milissegundos (2 segundos)
-    console.log(`Aguardando ${intervaloEntreAcessos / 1000} segundos até o próximo conjunto de acessos...`);
+    const intervaloEntreAcessos = 1000;
+    const timestamp = getCurrentTimestamp();
+    console.log(`${timestamp} Aguardando ${intervaloEntreAcessos / 1000} segundos até o próximo conjunto de acessos...`);
     await new Promise(resolve => setTimeout(resolve, intervaloEntreAcessos));
   }
 }
 
-// Inicie a simulação
 iniciarSimulacao();
